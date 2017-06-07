9420ec2e53f091ceacc059f5b3d44392a2e8ad6a
la <konkin_e@promis.ru>
2017-06-07 12:40:59 +0300

    add luastatus files

---
+++ luastatus/mem-free.lua
@@ -0,0 +1,20 @@
+f = assert(io.open('/proc/meminfo', 'r'))
+f:setvbuf('no')
+
+widget = {
+    plugin = 'timer',
+    opts = {period = 2},
+    cb = function(t)
+        f:seek('set', 0)
+        for line in f:lines() do
+            local key, value, unit = string.match(line, '(.*): +(.*) +(.*)')
+            if key == 'MemAvailable' then
+                if unit == 'kB' then
+                    value = value / 1024 / 1024
+                    unit = 'GiB'
+                end
+                return {full_text = string.format('  %3.2f %s', value, unit), color = '#af8ec3'}
+            end
+        end
+    end,
+}
